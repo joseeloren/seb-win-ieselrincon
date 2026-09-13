8470da0cff9da0f5230524fe2067e06406a67852
Jose <jose@example.com>
2026-09-13 12:05:50 +0100

    Bump version to 0.0.109

--- SafeExamBrowser.Applications.UnitTests/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Applications.UnitTests/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("fc6d80ec-8611-4287-87e2-17c028a10858")]
 
-[assembly: AssemblyVersion("0.0.108")]
-[assembly: AssemblyFileVersion("0.0.108")]
-[assembly: AssemblyInformationalVersion("0.0.108")]
+[assembly: AssemblyVersion("0.0.109")]
+[assembly: AssemblyFileVersion("0.0.109")]
+[assembly: AssemblyInformationalVersion("0.0.109")]
--- SafeExamBrowser.Client/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Client/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.108")]
-[assembly: AssemblyVersion("0.0.108")]
-[assembly: AssemblyFileVersion("0.0.108")]
-[assembly: AssemblyInformationalVersion("0.0.108")]
+// [assembly: AssemblyVersion("0.0.109")]
+[assembly: AssemblyVersion("0.0.109")]
+[assembly: AssemblyFileVersion("0.0.109")]
+[assembly: AssemblyInformationalVersion("0.0.109")]
