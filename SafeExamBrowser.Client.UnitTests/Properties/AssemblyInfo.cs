0ee7c9d8d3fb8f0ac0acbe508ff80276b339a86d
Jose <jose@example.com>
2026-09-13 10:06:54 +0100

    chore: synchronize client and installer version 0.0.103

--- SafeExamBrowser.Client.UnitTests/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Client.UnitTests/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("15684416-fadf-4c51-85de-4f343bfab752")]
 
-// [assembly: AssemblyVersion("0.0.100")]
-[assembly: AssemblyVersion("0.0.100")]
-[assembly: AssemblyFileVersion("0.0.100")]
-[assembly: AssemblyInformationalVersion("0.0.100")]
+// [assembly: AssemblyVersion("0.0.103")]
+[assembly: AssemblyVersion("0.0.103")]
+[assembly: AssemblyFileVersion("0.0.103")]
+[assembly: AssemblyInformationalVersion("0.0.103")]
--- SafeExamBrowser.I18n.UnitTests/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.I18n.UnitTests/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("9abb1bde-03e1-4076-8f8b-fda1e61ed675")]
 
-// [assembly: AssemblyVersion("0.0.100")]
-[assembly: AssemblyVersion("0.0.100")]
-[assembly: AssemblyFileVersion("0.0.100")]
-[assembly: AssemblyInformationalVersion("0.0.100")]
+// [assembly: AssemblyVersion("0.0.103")]
+[assembly: AssemblyVersion("0.0.103")]
+[assembly: AssemblyFileVersion("0.0.103")]
+[assembly: AssemblyInformationalVersion("0.0.103")]
