3b7305bfad428c871684582aa4e4f460d54889bb
Jose <jose@example.com>
2026-09-13 15:06:40 +0100

    Bump version to 0.0.113

--- SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.111")]
-[assembly: AssemblyFileVersion("0.0.111")]
-[assembly: AssemblyInformationalVersion("0.0.111")]
+[assembly: AssemblyVersion("0.0.113")]
+[assembly: AssemblyFileVersion("0.0.113")]
+[assembly: AssemblyInformationalVersion("0.0.113")]
