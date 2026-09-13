aaa21ca6c604a27d248348a27d1bd93d0ae0a692
Jose <jose@example.com>
2026-09-13 23:04:14 +0100

    v0.0.129: Rebuild installer to sync with version 0.0.129

--- SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.128")]
-[assembly: AssemblyFileVersion("0.0.128")]
-[assembly: AssemblyInformationalVersion("0.0.128")]
+[assembly: AssemblyVersion("0.0.129")]
+[assembly: AssemblyFileVersion("0.0.129")]
+[assembly: AssemblyInformationalVersion("0.0.129")]
