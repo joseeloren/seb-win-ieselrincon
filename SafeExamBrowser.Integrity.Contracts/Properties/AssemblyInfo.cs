e5204994a57c8d479eacee15f6f00e358db330f4
Jose <jose@example.com>
2026-09-13 23:14:28 +0100

    v0.0.130: Whitelist SEB window width and position keys for side-by-side tiling

--- SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.129")]
-[assembly: AssemblyFileVersion("0.0.129")]
-[assembly: AssemblyInformationalVersion("0.0.129")]
+[assembly: AssemblyVersion("0.0.130")]
+[assembly: AssemblyFileVersion("0.0.130")]
+[assembly: AssemblyInformationalVersion("0.0.130")]
