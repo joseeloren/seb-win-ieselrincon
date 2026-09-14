63e63f383d8431fe035ed049453de98df9c890d9
Jose <jose@example.com>
2026-09-14 08:29:07 +0100

    Bump version to 0.0.133 for proxy CONNECT 302 redirect

--- SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.132")]
-[assembly: AssemblyFileVersion("0.0.132")]
-[assembly: AssemblyInformationalVersion("0.0.132")]
+[assembly: AssemblyVersion("0.0.133")]
+[assembly: AssemblyFileVersion("0.0.133")]
+[assembly: AssemblyInformationalVersion("0.0.133")]
