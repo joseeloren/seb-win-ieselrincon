23f6ebe687ac109d968956ae9adfb003a73a2963
Jose <jose@example.com>
2026-09-14 11:07:34 +0100

    fix(v0.0.139): simplify lock screen heading and remove logo

--- SafeExamBrowser.Applications.UnitTests/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Applications.UnitTests/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("fc6d80ec-8611-4287-87e2-17c028a10858")]
 
-[assembly: AssemblyVersion("0.0.138")]
-[assembly: AssemblyFileVersion("0.0.138")]
-[assembly: AssemblyInformationalVersion("0.0.138")]
+[assembly: AssemblyVersion("0.0.139")]
+[assembly: AssemblyFileVersion("0.0.139")]
+[assembly: AssemblyInformationalVersion("0.0.139")]
--- SafeExamBrowser.Logging.UnitTests/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Logging.UnitTests/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("7f012305-1125-47ce-9c2a-146c891c0924")]
 
-// [assembly: AssemblyVersion("0.0.138")]
-[assembly: AssemblyVersion("0.0.138")]
-[assembly: AssemblyFileVersion("0.0.138")]
-[assembly: AssemblyInformationalVersion("0.0.138")]
+// [assembly: AssemblyVersion("0.0.139")]
+[assembly: AssemblyVersion("0.0.139")]
+[assembly: AssemblyFileVersion("0.0.139")]
+[assembly: AssemblyInformationalVersion("0.0.139")]
