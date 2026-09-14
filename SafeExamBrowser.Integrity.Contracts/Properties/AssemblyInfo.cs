25e48eb1a3b166a39047e7cc647e08e964f3487e
Jose <jose@example.com>
2026-09-14 08:17:11 +0100

    Bump version to 0.0.132 for proxy aesthetic changes

--- SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.131")]
-[assembly: AssemblyFileVersion("0.0.131")]
-[assembly: AssemblyInformationalVersion("0.0.131")]
+[assembly: AssemblyVersion("0.0.132")]
+[assembly: AssemblyFileVersion("0.0.132")]
+[assembly: AssemblyInformationalVersion("0.0.132")]
