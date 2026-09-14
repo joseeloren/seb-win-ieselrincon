71aa11c29a85c0808274b91900c9d811d15000c5
Jose <jose@example.com>
2026-09-14 09:42:23 +0100

    hotfix(v0.0.135): fix I18n XML encoding corruption, rename SEB -> El Rincon Seguro in translations

--- SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.134")]
-[assembly: AssemblyFileVersion("0.0.134")]
-[assembly: AssemblyInformationalVersion("0.0.134")]
+[assembly: AssemblyVersion("0.0.135")]
+[assembly: AssemblyFileVersion("0.0.135")]
+[assembly: AssemblyInformationalVersion("0.0.135")]
