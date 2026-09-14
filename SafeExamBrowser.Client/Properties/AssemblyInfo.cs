587724e962dec1805c88fca67d0c18edfdbe64d2
Jose <jose@example.com>
2026-09-14 17:34:46 +0100

    Synchronize client and installer version 0.0.144

--- SafeExamBrowser.Client/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Client/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.143")]
-[assembly: AssemblyVersion("0.0.143")]
-[assembly: AssemblyFileVersion("0.0.143")]
-[assembly: AssemblyInformationalVersion("0.0.143")]
+// [assembly: AssemblyVersion("0.0.144")]
+[assembly: AssemblyVersion("0.0.144")]
+[assembly: AssemblyFileVersion("0.0.144")]
+[assembly: AssemblyInformationalVersion("0.0.144")]
--- SafeExamBrowser.Proctoring/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Proctoring/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.143")]
-[assembly: AssemblyFileVersion("0.0.143")]
-[assembly: AssemblyInformationalVersion("0.0.143")]
+[assembly: AssemblyVersion("0.0.144")]
+[assembly: AssemblyFileVersion("0.0.144")]
+[assembly: AssemblyInformationalVersion("0.0.144")]
