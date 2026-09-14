89242d4b914a248626aab373211e854e99f1bac3
Jose <jose@example.com>
2026-09-14 23:50:50 +0100

    v0.0.151: bump version for new release

--- SafeExamBrowser.Service.UnitTests/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Service.UnitTests/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("26c4aaee-3902-400c-a154-63a357dea2f8")]
 
-// [assembly: AssemblyVersion("0.0.150")]
-[assembly: AssemblyVersion("0.0.150")]
-[assembly: AssemblyFileVersion("0.0.150")]
-[assembly: AssemblyInformationalVersion("0.0.150")]
+// [assembly: AssemblyVersion("0.0.151")]
+[assembly: AssemblyVersion("0.0.151")]
+[assembly: AssemblyFileVersion("0.0.151")]
+[assembly: AssemblyInformationalVersion("0.0.151")]
