274481d2b8b07c4ee74d7907b651a3ee2cf242a0
Jose <jose@example.com>
2026-09-15 08:41:15 +0100

    Sincronizar cliente e instalador 0.0.159

--- SafeExamBrowser.Integrity/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Integrity/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.158")]
-[assembly: AssemblyFileVersion("0.0.158")]
-[assembly: AssemblyInformationalVersion("0.0.158")]
+[assembly: AssemblyVersion("0.0.159")]
+[assembly: AssemblyFileVersion("0.0.159")]
+[assembly: AssemblyInformationalVersion("0.0.159")]
