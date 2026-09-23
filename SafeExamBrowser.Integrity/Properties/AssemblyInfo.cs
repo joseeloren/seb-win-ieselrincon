61afbf06eb50fcb434b85851b2a37bb1e723bc53
Jose <jose@example.com>
2026-09-24 00:03:35 +0100

    Sincronizar cliente e instalador 0.0.170

--- SafeExamBrowser.Integrity/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Integrity/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.169")]
-[assembly: AssemblyFileVersion("0.0.169")]
-[assembly: AssemblyInformationalVersion("0.0.169")]
+[assembly: AssemblyVersion("0.0.170")]
+[assembly: AssemblyFileVersion("0.0.170")]
+[assembly: AssemblyInformationalVersion("0.0.170")]
--- SafeExamBrowser.SystemComponents.Contracts/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.SystemComponents.Contracts/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.169")]
-[assembly: AssemblyVersion("0.0.169")]
-[assembly: AssemblyFileVersion("0.0.169")]
-[assembly: AssemblyInformationalVersion("0.0.169")]
+// [assembly: AssemblyVersion("0.0.170")]
+[assembly: AssemblyVersion("0.0.170")]
+[assembly: AssemblyFileVersion("0.0.170")]
+[assembly: AssemblyInformationalVersion("0.0.170")]
