8a5a32dc8e373a420fd1120eb03884cb77f0c82c
Jose <jose@example.com>
2026-09-24 01:03:29 +0100

    Sincronizar instalador con versión 0.0.174

--- SafeExamBrowser.Applications.UnitTests/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Applications.UnitTests/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("fc6d80ec-8611-4287-87e2-17c028a10858")]
 
-[assembly: AssemblyVersion("0.0.173")]
-[assembly: AssemblyFileVersion("0.0.173")]
-[assembly: AssemblyInformationalVersion("0.0.173")]
+[assembly: AssemblyVersion("0.0.174")]
+[assembly: AssemblyFileVersion("0.0.174")]
+[assembly: AssemblyInformationalVersion("0.0.174")]
--- SafeExamBrowser.Integrity/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Integrity/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.173")]
-[assembly: AssemblyFileVersion("0.0.173")]
-[assembly: AssemblyInformationalVersion("0.0.173")]
+[assembly: AssemblyVersion("0.0.174")]
+[assembly: AssemblyFileVersion("0.0.174")]
+[assembly: AssemblyInformationalVersion("0.0.174")]
