fb8d489d066a5a18e1bad3099cfa187e1c13df8f
Jose <jose@example.com>
2026-09-24 01:46:18 +0100

    Sincronizar cliente con versión 0.0.176

--- SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Integrity.Contracts/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.175")]
-[assembly: AssemblyFileVersion("0.0.175")]
-[assembly: AssemblyInformationalVersion("0.0.175")]
+[assembly: AssemblyVersion("0.0.176")]
+[assembly: AssemblyFileVersion("0.0.176")]
+[assembly: AssemblyInformationalVersion("0.0.176")]
