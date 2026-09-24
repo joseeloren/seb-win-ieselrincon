bf50a30e121709dc006fbfe6aab03f99c6491682
Jose <jose@example.com>
2026-09-24 02:58:29 +0100

    Sincronizar cliente con versión 0.0.179

--- SafeExamBrowser.Applications.UnitTests/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Applications.UnitTests/Properties/AssemblyInfo.cs
@@ -11,6 +11,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("fc6d80ec-8611-4287-87e2-17c028a10858")]
 
-[assembly: AssemblyVersion("0.0.178")]
-[assembly: AssemblyFileVersion("0.0.178")]
-[assembly: AssemblyInformationalVersion("0.0.178")]
+[assembly: AssemblyVersion("0.0.179")]
+[assembly: AssemblyFileVersion("0.0.179")]
+[assembly: AssemblyInformationalVersion("0.0.179")]
--- SafeExamBrowser.Runtime/Properties/AssemblyInfo.cs
+++ SafeExamBrowser.Runtime/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.178")]
-[assembly: AssemblyVersion("0.0.178")]
-[assembly: AssemblyFileVersion("0.0.178")]
-[assembly: AssemblyInformationalVersion("0.0.178")]
+// [assembly: AssemblyVersion("0.0.179")]
+[assembly: AssemblyVersion("0.0.179")]
+[assembly: AssemblyFileVersion("0.0.179")]
+[assembly: AssemblyInformationalVersion("0.0.179")]
